81921f6b1517e2e20799618fd3dc09af58481371
Irina Novakovich <irinaredhood@gmail.com>
2022-10-15 23:39:46 +0300

    2nd Block done

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./Header/Header";
 import Main from "./Main/Main";
+import SkillsBlock from "./Skills/SkillsBlock";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Main/>
+            <SkillsBlock/>
         </div>
     );
 }
---
+++ src/Skills/Skill/Skill.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Skill.module.css'
+
+type SkillPropsType = {
+    title: string
+    description: string
+}
+
+const Skill = (props: SkillPropsType) => {
+    return (
+        <div className={styles.skill}>
+            <div className={styles.icon}></div>
+            <h3>{props.title}</h3>
+            <span className={styles.description}>{props.description}</span>
+        </div>
+    );
+};
+
+export default Skill;
